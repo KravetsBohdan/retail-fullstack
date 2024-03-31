5b502a50b21c1026252781daf0ab2965a0d6efd4
BohdanKravets <bodykravets@gmail.com>
2024-03-31 12:47:06 +0300

    minor changes

--- backend/src/main/java/com/bkravets/springbootexample/Main.java
+++ backend/src/main/java/com/bkravets/springbootexample/Main.java
@@ -10,10 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class SpringBootExampleApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootExampleApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     @Bean
